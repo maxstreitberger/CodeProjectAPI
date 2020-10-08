a41e9ae06c7016cbf87e1d1c93486e0ce64cf308
Max Streitberger <max.streitberger@gmail.com>
2020-10-08 07:54:03 +0200

    Added schema file

--- src/index.js
+++ src/index.js
@@ -1,19 +1,13 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-const typeDefs = `
-type Query {
-  info: String!
-}
-`
-
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
+    info: () => `This is the API of my CODE project`
   }
 }
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers
 })
 server.start(() => console.log('🚀 Server is running on http://localhost:4000'))
