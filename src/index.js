17675afab63aedb75ed99adaedf3e6e9567a1da3
Max Streitberger <max.streitberger@gmail.com>
2020-10-08 08:55:34 +0200

    Added prisma queries to server

--- src/index.js
+++ src/index.js
@@ -3,7 +3,35 @@ const { PrismaClient } = require('@prisma/client')
 
 const resolvers = {
   Query: {
-    info: () => `This is the API of my CODE project`
+    info: () => `This is the API of my CODE project`,
+    events: async (parent, args, context) => {
+      return context.prisma.event.findMany()
+    }
+  },
+
+  Mutation: {
+    post: (parent, args, context, info) => {
+      const newEvent = context.prisma.event.create({
+        data: {
+          title: args.title,
+          date: args.data,
+          event_start: args.event_start,
+          description: args.description,
+          is_public: args.is_public,
+          event_type: args.event_type,
+          street_and_house_number: args.street_and_house_number,
+          city: args.city,
+          country: args.country,
+          user: {
+            connect: {
+              user_id: args.user_id
+            }
+          }      
+        }
+      })
+
+      return newEvent
+    }
   }
 }
 
