07be3094173be12b66e9255b7ff434b4840126e3
Max Streitberger <max.streitberger@gmail.com>
2020-10-28 07:36:30 +0100

    Added user permissions

--- api/permissions/index.ts
+++ api/permissions/index.ts
@@ -1,12 +1,15 @@
 import { shield } from 'graphql-shield'
 import { EventMutationPermissions, EventQueryPermissions } from '../events/event.permissions'
+import { UserMutationPermissions, UserQueryPermissions } from '../users/user.permissions'
 
 export const permissions = shield({
   Query: {
-    ...EventQueryPermissions
+    ...EventQueryPermissions,
+    ...UserQueryPermissions
   },
 
   Mutation: {
-    ...EventMutationPermissions
+    ...EventMutationPermissions,
+    ...UserMutationPermissions
   }
 })
---
+++ api/users/user.permissions.ts
@@ -0,0 +1,11 @@
+import { allow, not } from 'graphql-shield'
+import { rules } from '../permissions/rules'
+
+export const UserQueryPermissions = {
+  allUsers: rules.isAuthenticated
+}
+
+export const UserMutationPermissions = {
+  login: not(rules.isAuthenticated),
+  register: not(rules.isAuthenticated)
+}
